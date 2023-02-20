cddc6a8d603ba7911df4851caf6ae6fe44fa6c3d
Vladimir Zorov <vl.zorov@gmail.com>
2023-02-20 19:13:59 +0200

    add

--- src/Bombs.java
+++ src/Bombs.java
@@ -1,9 +1,21 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bombs {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+        ArrayDeque <Integer> bombEffects = new ArrayDeque<>();
+        ArrayDeque <Integer> bombCasing = new ArrayDeque<>();
+
+        Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).forEach(bombEffects::offer);
+        Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).forEach(bombCasing::push);
+
+
+        System.out.println();
+
     }
 }
